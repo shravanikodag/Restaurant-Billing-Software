3910ace34db00502057567036b92525efaa9796b
Shravani kodag <139679583+shravanikodag@users.noreply.github.com>
2025-08-18 19:45:11 +0530

    Update database_menu.py

--- database_menu.py.py
+++ database_menu.py.py
@@ -2,6 +2,13 @@ import sqlite3
 # 1.connect to database (it will create database folder &file if not exists)
 conn=sqlite3.connect('database/restaurant.db')
 cursor=conn.cursor()
+
+
+#CLEAR EXISTING MENU
+cursor.execute("DELETE FROM menu")
+cursor.execute("DELETE FROM sqlite_sequence WHERE name='menu'")
+
+
 #2.Create menu table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS menu(
@@ -10,34 +17,38 @@ item_name TEXT NOT NULL,
 category TEXT,
 price REAL NOT NULL,
 gst REAL)''')
+
 #3.sample menu
 menu_items=[
      ('Panner-tikka','Starter',150,5),
-     ('veg manchurian','starter',150,6),
-     ('veg soup','starter',150,2),
-     ('Panner butter  masala ','main course',150,6),
-     ('dal tadka','main course',100,5),
-     ('veg biryani','main course',350,6),
-     ('butter roti','bread',20,5),
-     ('paratha','bread',45,6),
-     ('butter naan','bread',30,4),
-     
+     ('Veg Manchurian','Starter',150,6),
+     ('Veg Soup','Starter',150,2),
+     ('Panner Butter Masala ','Main Course',150,6),
+     ('Dal Tadka','Main Course',100,5),
+     ('Veg Biryani','Main Course',350,6),
+     ('Butter Roti','Bread',20,5),
+     ('Paratha','Bread',45,6),
+     ('Butter Naan','Bread',30,4),
+     ('Jeera Rice','Rice',120,5),
+     ('Steam Rice','Rice',100,4),
+     ('Gulab Jamun','Desert',50,3),
+     ('Ice Cream','Desert',100,5),
+     ('Rasgulla','Desert',120,5)   
 ]
+
 #4.insert sample menu into  table
 cursor.executemany('''INSERT INTO MENU (item_name,category,price,gst)
 VALUES(?,?,?,?)''',menu_items)
+
 #5.save change
 conn.commit()
+
 #6.fetch and show all menu items
 cursor.execute("SELECT * FROM menu")
 rows=cursor.fetchall()
 print("MENU ITEMS:")
 for row in rows:
     print(row)
+    
 #7.close connection
-'''cursor.execute("DELETE FROM menu")
-cursor.execute("DELETE FROM sqlite_sequence WHERE name='menu'")
-
-conn.commit()
-print("deleted")'''
 conn.close()
