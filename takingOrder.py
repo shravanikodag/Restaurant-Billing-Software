e5daa2d468a3ff5558f9d2714eef791cb88518ed
Shravani kodag <139679583+shravanikodag@users.noreply.github.com>
2025-08-24 22:40:03 +0530

    Update takingOrder.py

--- takingOrder.py
+++ takingOrder.py
@@ -1,7 +1,7 @@
 Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-= RESTART: C:\Users\Aishwarya\Desktop\RestaurantBilling\Bill_generator.py
+= RESTART: C:\Users\\Desktop\RestaurantBilling\Bill_generator.py
 
 ======================================================================
                  🍽WELCOME TO THE TEST TERRACE RESTAURANT MENU🍽                  
